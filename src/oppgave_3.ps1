e8bb76fd020cd0a15548933ca7261fc08ca56cdc
mrkesu <sundby@gmail.com>
2022-01-28 13:05:44 +0100

    klasser! oh look at me im so fancy laa dee daa

---
+++ src/oppgave_3.ps1
@@ -0,0 +1,30 @@
+# Leker meg litt med klasser for å lære hvordan de funker...
+class Deck {
+    [array]$cards
+
+    # Lager en egen tostring for å løse oppgaven enkelt
+    [string]ToString(){
+        $out = ""
+        for ($i = 0; $i -lt $this.cards.Length; $i++) {
+            $out += $this.cards[$i].suit.Substring(0, 1) + $this.cards[$i].value
+
+            # Legg til komma etter hver iterasjon, men ikke til slutt
+            if($i -lt ($this.cards.Length - 1)) {
+                $out += ","
+            }
+        }
+
+        return "Kortstokk: $out"
+    }
+
+    # Constructor, starter med en ferdig stokket kortstokk
+    Deck() {
+        $request = Invoke-WebRequest -Uri "http://nav-deckofcards.herokuapp.com/shuffle";
+        $this.cards = $request.Content | ConvertFrom-Json
+    }
+}
+
+$deck = [Deck]::new()
+
+# ToString() automagi ╰(*°▽°*)╯
+Write-Host $deck
