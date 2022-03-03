abe6b503d1ada09616c9d5464957601452ea1bc8
mrkesu <sundby@gmail.com>
2022-03-03 12:36:00 +0100

    smårydding i koden

--- src/oppgave_10.ps1
+++ src/oppgave_10.ps1
@@ -6,13 +6,30 @@ param (
 
 # Leker meg litt med klasser for å lære hvordan de funker...
 class Deck {
-    [object]$cards
+    [array]$cards
 
-    # Lager en egen tostring for å løse oppgaven enkelt
+    # Tom kortstokk (f.eks. en spiller sin starthand)
+    Deck() {
+        $this.cards = @()
+    }
+
+    # Kortstokk hentet fra URL
+    Deck([System.URI]$URL) {
+        try {
+            $request = Invoke-WebRequest -Uri $URL
+            $this.cards = $request.Content | ConvertFrom-Json
+        }
+        catch {
+            Write-Error "Klarte ikke å hente kortstokk :("
+            Throw $_.Exception.Message
+        }
+    }
+
+    # Egendefinert ToString for å forenkle output av kortstokken
     [string]ToString() {
         $out = ""
         for ($i = 0; $i -lt $this.cards.Length; $i++) {
-            $out += $this.cards[$i].suit.Substring(0, 1) + $this.cards[$i].value
+            $out += $this.cards[$i].suit[0] + $this.cards[$i].value
 
             # Legg til komma etter hver iterasjon, men ikke til slutt
             if ($i -lt ($this.cards.Length - 1)) {
@@ -25,24 +42,18 @@ class Deck {
     # Samlet poengsum for kortene i kortstokk.
     # Knekt (J), Dronning (Q) og Konge (K) teller som 10 poeng, Ess (A) teller som 11 poeng, resten er vanlig
     [int]GetPoints() {
-        [int]$i = 0
-        try {
-            foreach ($card in $this.cards) {
-                $i += switch ($card.value) {
-                    { $_ -in 'J', 'Q', 'K' } { 10 }
-                    'A' { 11 }
-                    Default { [int]::Parse($card.value) }
-                }
+        $i = 0
+        foreach ($card in $this.cards) {
+            $i += switch ($card.value) {
+                { $_ -in 'J', 'Q', 'K' } { 10 }
+                'A' { 11 }
+                Default { [int]::Parse($card.value) }
             }
-        }
-        catch {
-            Write-Error "Får ikke til å kalkulere poengsummen, hjelp!"
-            Throw $_.Exception.Message
         }
         return $i
     }
 
-    # Trekke kort fra toppen av kortstokken
+    # Trekke kort fra toppen av denne kortstokken og legge det til i en annen kortstokk
     [void]GetCard([int]$amount, [Deck]$hand) {
         for ($i = 0; $i -lt $amount; $i++) {
             # Lagre øverste kort
@@ -60,31 +71,13 @@ class Deck {
             $hand.cards += $topCard
         }
     }
-
-    # Constructor med en ferdig stokket kortstokk
-    Deck([System.URI]$URL) {
-        #$request = Invoke-WebRequest -Uri "http://nav-deckofcards.herokuapp.com/shuffle";
-        try {
-            $request = Invoke-WebRequest -Uri $URL
-            $this.cards = $request.Content | ConvertFrom-Json
-        }
-        catch {
-            Write-Error "Klarte ikke å hente kortstokk :("
-            Throw $_.Exception.Message
-        }
-    }
-
-    # Constructor med en tom kortstokk
-    Deck([object]$hand) {
-        $this.cards = @()
-    }
 }
 
 class Player {
-    [string]$name                       # spillernavn
-    [object]$hand = [Deck]::new($hand)  # spiller sine kort, en "tom kortstokk"
-    [object]$deck                       # referanse til kortstokken som brukes
-    [bool]$active = $true
+    [string ]$name                  # spillernavn
+    [Deck   ]$hand = [Deck]::new()  # spiller sine kort, en "tom kortstokk"
+    [Deck   ]$deck                  # referanse til kortstokken som brukes
+    [bool   ]$active = $true
 
     Player($name, [Deck]$deck) {
         $this.name = $name
@@ -140,17 +133,16 @@ class Blackjack {
 
 # Starte spillet + lage kortstokk-variabelen de ønsker i oppgaven
 $game = [Blackjack]::new()
-$kortstokk = $game.deck
 
 # Skrive ut kortstokk
-Write-Host "Kortstokk: $kortstokk"
-Write-Host "Poengsum: $($kortstokk.GetPoints())"
+Write-Host "Kortstokk: $($game.deck)"
+Write-Host "Poengsum: $($game.deck.GetPoints())"
 Write-Host ""
 
-# Skape to nye spillere i blackjack med referanse til kortstokken
+# Opprette to nye spillere
 $game.newPlayer("meg") | Out-Null
 $game.newPlayer("magnus") | Out-Null
-#$player3 = $game.newPlayer("spiller3")
+#$game.newPlayer("spiller3") | Out-Null
 
 # En vinner funnet
 if($game.Winner().Length -eq 1) {
@@ -190,4 +182,4 @@ foreach($player in ($game.players | Sort-Object -Property name)) {
 
 # Skriver ut kortstokken igjen
 Write-Host ""
-Write-Host "Kortstokk: $kortstokk"
+Write-Host "Kortstokk: $($game.deck)"
